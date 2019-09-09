98c8a55cb7f8cd5202725170656fb14f4af86ff3
Matt Davenport <matt.davenport@bss-llc.com>
2019-09-09 13:37:05 -0400

    Add Admin REST class for ExtraRESTful admin access

---
+++ code/Model/Api2/Product/Rest/Admin/V2.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @author Daniel Deady <daniel@clockworkgeek.com>
+ * @license MIT
+ */
+class Clockworkgeek_Extrarestful_Model_Api2_Product_Rest_Admin_V2 extends Clockworkgeek_Extrarestful_Model_Api2_Product
+{
+}
